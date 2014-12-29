f4a38b07ccbf1534087b9511c602b1cde61849a8
alb <alberto1337@gmail.com>
2014-12-29 17:10:32 +0100

    weather_extract.py updated

--- weather_extract.py
+++ weather_extract.py
@@ -4,41 +4,28 @@
 
 import xml.etree.ElementTree as etree
 import urllib2
+from datetime import date
 
 ## Code starts here
+
+#We obtains today data
+todaydate = date.today()
 
 #Download of the xml
 xml_url = "http://www.aemet.es/xml/municipios/localidad_28079.xml"
 xml_content = urllib2.urlopen(xml_url)
-#xml_content = s.read()
 
+#we obtain the whole XML tree
 tree = etree.parse(xml_content)
 root = tree.getroot()
 
-
-info_dia = root.find(".//dia[@fecha='2014-12-08']/temperatura")
+#After converting the date string, we check the required fields
+str_infodia = ".//dia[@fecha='" + str(todaydate) + "']/temperatura"
+info_dia = root.find(str_infodia)
 print info_dia.find('minima').text
 print info_dia.find('maxima').text
 
-"""
-#for temp in root.iter(".//dia[@fecha='2014-12-08']"):
-for temp in root.iter("dia"):
-    print temp.get('fecha')
-    for temp2 in temp.iter('temperatura'):
-        print temp2.find('maxima').text
-        print temp2.find('minima').text
-        
-
-for temp in root.iter('temperatura'):
-    t_max = temp.find('maxima').text
-    t_min = temp.find('minima').text
-    print t_max, t_min
-    
+for node in info_dia.iter('dato'):
+    print "Temperatura periodo %s: %s" % (node.attrib.get('hora'), node.text)
 
 
-#for temp in root.iter('temperatura'):
-#    print temp.attrib, temp.text
-
-#for child in root:
-#        print child.tag, child.attrib
-"""
